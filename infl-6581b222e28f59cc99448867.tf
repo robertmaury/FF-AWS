086c41d4489414ba5af861141f0fe6dc097147d3
Robert Maury <daquinox@gmail.com>
2023-12-19 10:09:23 -0500

    Codification: aws_lambda_function

---
+++ infl-6581b222e28f59cc99448867.tf
@@ -0,0 +1,21 @@
+import {
+  to = aws_lambda_function.radio-export-430
+  id = "radio-export"
+}
+
+
+resource "aws_lambda_function" "radio-export-430" {
+  architectures = ["x86_64"]
+  function_name = "radio-export"
+  handler       = "lambda_function.lambda_handler"
+  role          = "arn:aws:iam::821020995254:role/service-role/radio-scraper-role-s6pbe61t"
+  runtime       = "python3.7"
+  tracing_config {
+    mode = "PassThrough"
+  }
+  # The following attributes have default values introduced when importing the resource into terraform: [publish timeouts]
+  lifecycle {
+    ignore_changes = [publish, timeouts]
+  }
+}
+
